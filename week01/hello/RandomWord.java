cae653c3ccfd0fa0f6c6eac662cafba218906bc7
Murilo Antas Pereira <murilo.antas@gmail.com>
2024-01-25 17:48:53 -0300

    initial commit

---
+++ week01/hello/RandomWord.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        var count = 0;
+        String champion = "";
+        // Read the strings from the standard input
+        while (!StdIn.isEmpty()) {
+            count++;
+            var word = StdIn.readString();
+            champion = StdRandom.bernoulli(1 / (double) count) ? word : champion;
+        }
+        StdOut.println(champion);
+    }
+}
